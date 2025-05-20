6d099f1af2673e9424722dac3aef6cab12bc0e46
over-over-STEAK <ljiawei255@gmail.com>
2025-05-20 14:47:54 +0800

    0.1.js

---
+++ homework/HW.3/0.1.js
@@ -0,0 +1,9 @@
+function countLetters(str) {
+  const letterMap = new Map();
+  for (const char of str) {
+    if (/^[a-zA-Z]$/.test(char)) {
+      letterMap.set(char, (letterMap.get(char) || 0) + 1);
+    }
+  }
+  return letterMap;
+}
